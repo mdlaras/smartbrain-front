147393b471030079e8254845802c7b6a4c5ff18a
mdlaras <mariadlarasati@gmail.com>
2020-07-29 18:04:06 +0700

    add several components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
+import Rank from './components/rank/Rank'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */} 
+      <Rank/>
+      <ImageLinkForm />
+      {/* <FaceRecognition />  */}
     </div>
   );
 }
