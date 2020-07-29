8826ac1e914f717cc18f3e0f04ff45a81e163f3c
mdlaras <mariadlarasati@gmail.com>
2020-07-29 18:44:24 +0700

    finishing components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js'
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
 import Rank from './components/rank/Rank'
@@ -8,6 +9,19 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 function App() {
   return (
     <div className="App">
+       <Particles 
+              className='particles'
+              params={{
+            		particles: {
+                  number:{
+                    value:30,
+                    density:{
+                      enable:true,
+                      value_area:800
+                    }
+                  }}
+              }}
+            />
       <Navigation/>
       <Logo />
       <Rank/>
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Rank = () => {
     return(
        <div>
-           <div className='white f3'>
+           <div className='f3'>
                 {'Laras, your current rank is...'}
            </div>
-           <div className='white f1'>
+           <div className='f1'>
                 {'#5'}
            </div>
        </div>
