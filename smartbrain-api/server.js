be379e432489e5c3614462d55021edb61fa57e29
mdlaras <mariadlarasati@gmail.com>
2020-08-07 15:00:22 +0700

    make database

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -2,8 +2,32 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+    users: [
+        {
+            id:'`123',
+            name:'john',
+            email:'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id:'`124',
+            name:'sarah',
+            email:'sarah@gmail.com',
+            password: 'icecream',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 app.get('/', (req, res)=>{
     res.send('this is working')
+})
+
+app.post('/signin', (req,res)=> {
+    res.json('')
 })
 
 app.listen(3000, ()=> {
