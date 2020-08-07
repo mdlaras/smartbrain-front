f9ba7bae0dbb2cf57def03abd68c398ab551953e
mdlaras <mariadlarasati@gmail.com>
2020-08-07 15:52:46 +0700

    make counter

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -57,7 +57,7 @@ app.get('/profile/:id', (req,res) =>{
     database.users.forEach(user => {
         if (user.id === id){
             found = true;
-            return res.send(user);
+            return res.json(user);
         }
     })
     if(!found){
@@ -65,6 +65,20 @@ app.get('/profile/:id', (req,res) =>{
     }
 })
 
+app.post('/image', (req,res)=> {
+    const { id }  = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id){
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found')
+    }
+})
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 })
