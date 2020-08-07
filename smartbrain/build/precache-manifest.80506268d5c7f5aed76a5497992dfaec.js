cb2fd98191818f9a7440f7aee3d043d9bee59f09
mdlaras <mariadlarasati@gmail.com>
2020-08-07 14:29:25 +0700

    cleaning up

---
+++ smartbrain/build/precache-manifest.80506268d5c7f5aed76a5497992dfaec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd146dba63113396387286cd76c511ec",
+    "url": "/facedetector/index.html"
+  },
+  {
+    "revision": "cdaaef86339e0c3a7bc0",
+    "url": "/facedetector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "315d8e3f812344bcc026",
+    "url": "/facedetector/static/css/main.e75023ff.chunk.css"
+  },
+  {
+    "revision": "cdaaef86339e0c3a7bc0",
+    "url": "/facedetector/static/js/2.60a9acc5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetector/static/js/2.60a9acc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315d8e3f812344bcc026",
+    "url": "/facedetector/static/js/main.0fe8d722.chunk.js"
+  },
+  {
+    "revision": "b03125bd5c9861ab5e2f",
+    "url": "/facedetector/static/js/runtime-main.dafb8492.js"
+  },
+  {
+    "revision": "28ddcab44805c08e941925a1e4a7a3f1",
+    "url": "/facedetector/static/media/brain.28ddcab4.png"
+  }
+]);
